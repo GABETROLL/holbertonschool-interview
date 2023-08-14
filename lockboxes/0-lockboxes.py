6f502bbf505690928dc57d3fea2443f227286019
GABETROLL <gabriel7icplhappy@gmail.com>
2023-08-14 10:11:37 -0400

    fix pep8 style

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
 You have n number of locked boxes in front of you.
-Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+Each box is numbered sequentially from 0 to n - 1
+    and each box may contain keys to the other boxes.
 Box 0 is unlocked, and every other box is.
 
-Not all the keys are guaranteed to open boxes in the range of the list.
+- Not all the keys are guaranteed to open boxes in the range of the list.
 
 Write a method that determines if all the boxes can be opened.
 """
