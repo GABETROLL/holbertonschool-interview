32eb2c7b39ddb8474e3c17d76e13614b1550c765
GABETROLL <gabriel7icplhappy@gmail.com>
2023-09-04 20:32:41 -0400

    Add minimum_operations/

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3.11
+"""
+In a text file, there is a single character H.
+Only by using Ctrl+a and Ctrl+v,
+what is the least amount of commands
+necessary to make the text file have n H's?
+"""
+from math import sqrt
+
+
+def minOperations(n: int) -> int:
+    """
+    Returns the amount of commands (Ctrl+a or Ctrl+v)
+    it takes to transform a text file with one H
+    to have n H's.
+
+    If the aount of H's 'n' is not possible
+    to be achieved (negative or 0 H amounts),
+    this function returns 0.
+
+    (NOTE THAT FOR 1 H, THE RESUT IS ALSO 0!)
+    """
+
+    if n < 1:
+        return 0
+
+    # base case
+    if n == 1:
+        return 0
+
+    # a is the 2nd biggest factor of n,
+    # and b is the biggest factor of n.
+
+    a: int = int(sqrt(n))
+
+    while n / a % 1 != 0:
+        a -= 1
+    b: int = n // a
+
+    return minOperations(a) + b
+
+    """
+    1  2  3  4  5  6  7  8  9  10  11  12  13  14  15  16  17  18  19  20
+
+    0, 2,    4,    5,    6,     7,      8,      9,     10,     11,     12...
+    0,    3,       5,       6,          7,          8,          9...
+    0,       4,          6,             7,              8,              9...
+    0,          5,              7,                  8,                  9...
+    0,             6,                   8,                      9...
+
+    0 H 2 HH 4 HHHH 5 HHHHHH 6 HHHHHHHH 7 HHHHHHHHHH 8 HHHHHHHHHHHH
+    0 H 3 HHH 5 HHHHHH 6 HHHHHHHHH 7 
+
+    After looking at the data, turns out it's a prime factorization, which
+    I believe is an NP-hard problem.
+
+    THE TWO STARTING FACTORS SHOULD BE THE TWO FACTORS CLOSEST
+    TO ITS SQUARE ROOT (which may sometimes be the square root twice).
+
+    Examples:
+
+    20H is just copying 5H and pasting it 4 times
+    35H is just copying 7H and pasting it 5 times
+
+    100H is just copying 10H and pasting it 10 times;
+        10H is just copying 5H and pasting it 2H times
+
+    ALL PRIME NUMBER P*H IS JUST COPYING H AND PASTING IT P - 1 TIMES.
+    """
+
---
+++ minimum_operations/README.md
@@ -0,0 +1,5 @@
+# Minimum Operations
+Imagine you're editing a text file with a single 'H' character.
+You're only allowed to Ctrl+a (COPY ALL) or Ctrl+v (PASTE).
+Make a function that returns the least amount of commands to transform the file with a single H into a file with any integer amount of H's.
+
