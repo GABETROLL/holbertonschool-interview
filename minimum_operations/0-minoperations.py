32268f7984c09fc406ae0f8ecfd37678fd557bf3
GABETROLL <gabriel7icplhappy@gmail.com>
2023-09-04 20:49:13 -0400

    Move docstring up

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -4,7 +4,31 @@ In a text file, there is a single character H.
 Only by using Ctrl+a and Ctrl+v,
 what is the least amount of commands
 necessary to make the text file have n H's?
-"""
+
+    Here's me trying to figure it out:
+
+    1  2  3  4  5  6  7  8  9  10  11  12  13  14  15  16  17  18  19  20
+
+    0, 2,    4,    5,    6,     7,      8,      9,     10,     11,     12...
+    0,    3,       5,       6,          7,          8,          9...
+    0,       4,          6,             7,              8,              9...
+    0,          5,              7,                  8,                  9...
+    0,             6,                   8,                      9...
+
+    0 H 2 HH 4 HHHH 5 HHHHHH 6 HHHHHHHH 7 HHHHHHHHHH 8 HHHHHHHHHHHH
+    0 H 3 HHH 5 HHHHHH 6 HHHHHHHHH 7
+
+    After looking at the data, turns out it's a type of factorization.
+    Examples:
+
+    20H is just copying 5H and pasting it 4 times
+    35H is just copying 7H and pasting it 5 times
+
+    100H is just copying 10H and pasting it 10 times;
+        10H is just copying 5H and pasting it 2H times
+
+    ALL PRIME NUMBER P*H IS JUST COPYING H AND PASTING IT P - 1 TIMES.
+    """
 from math import sqrt
 
 
@@ -39,31 +63,4 @@ def minOperations(n):
 
     return minOperations(a) + b
 
-    """
-    1  2  3  4  5  6  7  8  9  10  11  12  13  14  15  16  17  18  19  20
-
-    0, 2,    4,    5,    6,     7,      8,      9,     10,     11,     12...
-    0,    3,       5,       6,          7,          8,          9...
-    0,       4,          6,             7,              8,              9...
-    0,          5,              7,                  8,                  9...
-    0,             6,                   8,                      9...
-
-    0 H 2 HH 4 HHHH 5 HHHHHH 6 HHHHHHHH 7 HHHHHHHHHH 8 HHHHHHHHHHHH
-    0 H 3 HHH 5 HHHHHH 6 HHHHHHHHH 7
-
-    After looking at the data, turns out it's a prime factorization, which
-    I believe is an NP-hard problem.
-
-    THE TWO STARTING FACTORS SHOULD BE THE TWO FACTORS CLOSEST
-    TO ITS SQUARE ROOT (which may sometimes be the square root twice).
-
-    Examples:
-
-    20H is just copying 5H and pasting it 4 times
-    35H is just copying 7H and pasting it 5 times
-
-    100H is just copying 10H and pasting it 10 times;
-        10H is just copying 5H and pasting it 2H times
-
-    ALL PRIME NUMBER P*H IS JUST COPYING H AND PASTING IT P - 1 TIMES.
-    """
+    
