3a862a08660dc585f18a18b27d47ddcdbe9e4a4f
GABETROLL <gabriel7icplhappy@gmail.com>
2023-09-20 15:33:33 -0400

    Fix betty

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -4,10 +4,9 @@
 
 /**
  * add_nodeint_start - Adds a new node of type
- * 'listint_t', at the beggining of '*head':
- *  making the new node's 'next' be '*head'
- *  and replacing '*head' with the new node pointer.
- *
+ * 'listint_t', at the beggining of '*head'
+ * (making the new node's 'next' be '*head'
+ * and replacing '*head' with the new node pointer).
  * This function then returns that node.
  * THE NODE MUST BE FREED WITH
  * free(<returned node address>)
@@ -26,17 +25,18 @@
  *
  * Return: address of the new node, or NULL if
  * this function failed.
- *
  * IF THE RETURNED NODE ADDRESS ISN'T NULL,
  * THE MEMORY SPACE FOR THE NODE MUST BE FREED
  * WITH free(<returned node address>)
  */
 listint_t *add_nodeint_start(listint_t **head, const int n)
 {
+	listint_t *new;
+
 	if (!head)
 		return (NULL);
 
-	listint_t *new = malloc(sizeof(listint_t));
+	new = malloc(sizeof(listint_t));
 
 	if (!new)
 		return (NULL);
@@ -52,7 +52,6 @@ listint_t *add_nodeint_start(listint_t **head, const int n)
 /**
  * is_palindrome - Calculates
  * weather or not '*head' is a palindrome.
- *
  * If we go through each node in '*head',
  * and we make anothe linked list that's the
  * reverse of '*head', we can then make
@@ -61,11 +60,9 @@ listint_t *add_nodeint_start(listint_t **head, const int n)
  *
  * @head: pointer to pointer of the head node of
  * the linked list of type 'listint_t'.
- *
  * (I think visualizing the pointer to each node
  * as part of the node itself makes it easier to think
  * about this problem)
- *
  * IF 'head' IS NULL, THIS FUNCTION JUST RETURNS NULL
  * BEFORE DOING ANTHING.
  *
@@ -75,33 +72,31 @@ listint_t *add_nodeint_start(listint_t **head, const int n)
  */
 int is_palindrome(listint_t **head)
 {
+	/* reversed version of '*head' */
+	listint_t *reversed = NULL;
+	/* for scanning the original linked list and the reversed one */
+	listint_t **original_node;
+	listint_t **reversed_node;
+
 	if (!head || !(*head))
 		return (0);
-
 	/*
 	 * BUILD a reversed version of '*head'
 	 * stored in '*reversed'
 	 */
-	listint_t *reversed = NULL;
-	listint_t **node = head;
-
-	while (*node)
+	original_node = head;
+	while (*original_node)
 	{
-		add_nodeint_start(&reversed, (*node)->n);
-		node = &((*node)->next);
+		add_nodeint_start(&reversed, (*original_node)->n);
+		original_node = &((*original_node)->next);
 	}
-
 	/*
 	 * Comparing the reversed sequence with the original.
 	 * Returns 1 if all of the n's match, and 0 if they dont'.
 	 */
-	listint_t **original_node = head;
-	listint_t **reversed_node = &reversed;
-
-	/*
-	 * Both linked lists should be the same length,
-	 * since one is just the reverse of the other.
-	 */
+	original_node = head;
+	reversed_node = &reversed;
+	/* If we reached the end of one, the other one should have ended too */
 	while ((*original_node) && (*reversed_node))
 	{
 		if ((*original_node)->n != (*reversed_node)->n)
@@ -110,10 +105,11 @@ int is_palindrome(listint_t **head)
 		original_node = &((*original_node)->next);
 		reversed_node = &((*reversed_node)->next);
 	}
-
 	if (*original_node || *reversed_node)
-		puts("Original and reversed have different lengths!!!");
-
+	{
+		fputs("Original and reversed have different lengths!!!", stderr);
+		exit(1);
+	}
 	return (1);
 }
 
